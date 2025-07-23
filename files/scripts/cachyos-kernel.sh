8120e6687717ffe443515e6f05337b60aff88128
Nathan <ndowens@artixlinux.org>
2025-07-22 23:45:49 -0500

    Fix?

--- files/scripts/cachyos-kernel.sh
+++ files/scripts/cachyos-kernel.sh
@@ -9,7 +9,7 @@ log "Installing cachyos kernel"
 
 dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
 
-rpm-ostree override remove akmods kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel-matched
+dnf5 -y remove akmods kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel-matched
 dnf5 -y install akmods kernel-cachyos-lto kernel-cachyos-lto-modules kernel-cachyos-lto-core \
 	kernel-cachyos-lto-devel-matched
  
