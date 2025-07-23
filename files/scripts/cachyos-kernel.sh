af29868ab838831a19dabdc038b4348317da6125
Nathan <ndowens@artixlinux.org>
2025-07-23 03:01:03 -0500

    Uncomment kernel installation commands in cachyos-kernel.sh and add script to recipe.yml

--- files/scripts/cachyos-kernel.sh
+++ files/scripts/cachyos-kernel.sh
@@ -7,11 +7,11 @@ log() {
 
 log "Installing cachyos kernel"
 
-dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
+#dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
 
-dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel-matched
-dnf5 -y install akmods kernel-cachyos-lto kernel-cachyos-lto-modules kernel-cachyos-lto-core \
-	kernel-cachyos-lto-devel-matched
+#dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel-matched
+#dnf5 -y install akmods kernel-cachyos-lto kernel-cachyos-lto-modules kernel-cachyos-lto-core \
+#	kernel-cachyos-lto-devel-matched
 
-dnf5 -y copr disable bieszczaders/kernel-cachyos-lto
+#dnf5 -y copr disable bieszczaders/kernel-cachyos-lto
 setsebool -P domain_kernel_load_modules on
